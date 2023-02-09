e04efe105517df0aa00409c62218dcab93fe189f
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-02-08 20:30:46 -0500

    added hidden menu on feed-burger

--- 51-clone-website/script.js
+++ 51-clone-website/script.js
@@ -1,5 +1,30 @@
 const burger = document.getElementById('burger');
-const hiddenMenu = document.getElementById('hidden-menu')
+const hiddenMenu = document.getElementById('hidden-menu');
+const hiddenLayer = document.querySelector('.hidden-layer');
+const feedBurger = document.querySelector('.feed-burger');
+const hiddenFM = document.querySelector('.hidden-feed-menu');
 burger.addEventListener('click', () => {
   hiddenMenu.classList.toggle('hidden');
-})
+  hiddenLayer.classList.toggle('hidden');
+});
+
+hiddenLayer.addEventListener('click', () => {
+  hiddenMenu.classList.toggle('hidden');
+  hiddenLayer.classList.toggle('hidden');
+});
+
+feedBurger.addEventListener('mouseenter', () => {
+  hiddenFM.classList.remove('hidden');
+});
+
+feedBurger.addEventListener('mouseleave', () => {
+  hiddenFM.classList.add('hidden');
+});
+
+hiddenFM.addEventListener('mouseenter', () => {
+  hiddenFM.classList.remove('hidden');
+});
+
+hiddenFM.addEventListener('mouseleave', () => {
+  hiddenFM.classList.add('hidden');
+}); 
